5d9769a920398495a5f0dd8fec04a7e820bd8fa5
Noa Druesne <noa.druesne7@gmail.com>
2023-03-31 11:07:40 +0200

    End of Day1 Part 2

--- src/Controller/DataController.php
+++ src/Controller/DataController.php
@@ -8,17 +8,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DataController extends AbstractController
 {
-    #[Route('showValue', name: 'SHOW_VALUE')]
+    #[Route('/', name: 'SHOW_VALUE')]
     public function showValue(): Response
     {
         $maxValue = $this->getMaxValue();
+        $amount = $this->getAmount();
 
         return $this->render('data.html.twig', [
             'max_value' => $maxValue,
+            'amount' => $amount,
         ]);
     }
 
-    #[Route('/data')]
     public function getDataFromFile(): array
     {
         $file = file_get_contents('/home/noa/PhpstormProjects/AdventsOfCode/Day1/src/data.txt');
@@ -26,7 +27,6 @@ class DataController extends AbstractController
         return explode("\n\n", $file);
     }
 
-    #[Route('/modifyData')]
     public function getMaxValue(): int
     {
         $file = $this->getDataFromFile();
@@ -39,5 +39,22 @@ class DataController extends AbstractController
             $sum[$key] = array_sum($array[$key]);
         }
         return max($sum);
+    }
+
+    public function getAmount(): int
+    {
+        $file = $this->getDataFromFile();
+
+        $array = [];
+        $sum = [];
+
+        foreach ($file as $key => $data) {
+            $array[$key] = preg_split("/\s/", $data);
+            $sum[$key] = array_sum($array[$key]);
+        }
+
+        rsort($sum);
+
+        return array_sum(array_slice($sum,0,3));
     }
 }
